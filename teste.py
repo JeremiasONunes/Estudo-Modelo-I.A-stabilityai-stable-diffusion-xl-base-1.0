987ff1c90de296a38dd7441ff335c5f92b901f3c
Jeremias De Oliveira Nunes <104111596+JeremiasONunes@users.noreply.github.com>
2024-12-06 11:17:48 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import InferenceClient
-client = InferenceClient("stabilityai/stable-diffusion-xl-base-1.0", token="Bearer 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")
-
-# output is a PIL.Image object
-image = client.text_to_image("Astronaut riding a horse")
-image.save("/c:/Users/Jerem/OneDrive/Área de Trabalho/SDXL/generated_image.png")
